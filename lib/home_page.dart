ed3ce362130c9e8e2c4eb65aa810a4b042eccbdd
Aaqil Shihab <aaqilsh@yahoo.com>
2021-09-27 00:54:02 +0530

    Added translate view

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,6 +11,45 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Translate'),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 25.0,
+          ),
+          // Center(child: Text('Enter text')),
+          const Flexible(
+            child: TextField(
+              textAlign: TextAlign.center,
+              maxLengthEnforcement: MaxLengthEnforcement.none,
+              expands: false,
+              maxLines: 12,
+              minLines: null,
+              decoration: InputDecoration.collapsed(
+                hintText: 'Enter text to translate',
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          Center(
+            child: TextButton(
+              child: const Text('Translate'),
+              onPressed: () {},
+            ),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          const SelectableText('Result')
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:translate/home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
