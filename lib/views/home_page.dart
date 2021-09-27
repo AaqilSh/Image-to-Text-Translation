ba36d61ebd9754f735da28620c280c04f1ee9836
Aaqil Shihab <aaqilsh@yahoo.com>
2021-09-27 17:49:33 +0530

    Added style to buttos

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:translate/views/image_translate.dart';
+import 'package:translate/views/text_translate.dart';
 
 import 'widgets/select_button.dart';
 
@@ -15,16 +17,25 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Translate'),
+        title: const Text('Translate App'),
         centerTitle: true,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SelectButton(),
-            MaterialButton(onPressed: () {}, child: const Text('From image'))
+          children: const [
+            SelectButton(
+              label: 'From text',
+              page: TextTranslatepage(),
+            ),
+            SizedBox(
+              height: 12.0,
+            ),
+            SelectButton(
+              label: 'From image',
+              page: ImageTranslatePage(),
+            )
           ],
         ),
       ),
--- lib/views/widgets/select_button.dart
+++ lib/views/widgets/select_button.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class SelectButton extends StatelessWidget {
+  final String label;
+  final Widget? page;
   const SelectButton({
+    this.page,
+    required this.label,
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialButton(
-      onPressed: () {},
-      child: const Text('Text translate'),
-      textColor: Colors.red,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(
-          Radius.circular(12.0),
-        ),
-      ),
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(context, MaterialPageRoute(builder: (_) => page!));
+      },
+      child: Text(label),
+      style: ButtonStyle(
+          alignment: Alignment.center,
+          backgroundColor: MaterialStateProperty.all(Colors.deepPurple[900]),
+          elevation: MaterialStateProperty.all(12.0)),
     );
   }
 }
