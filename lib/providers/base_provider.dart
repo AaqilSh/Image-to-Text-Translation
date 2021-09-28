189206942306a0b73b7b1f0c6a359531f2fce7b6
Aaqil Shihab <aaqilsh@yahoo.com>
2021-09-28 13:29:51 +0530

    Setup base provider

---
+++ lib/providers/base_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:translate/services/status.dart';
+
+class BaseProvider extends ChangeNotifier {
+  Status _currentStatus = Status.idle;
+
+  Status get currentStatus => _currentStatus;
+
+  void setStatus(Status status) {
+    _currentStatus = status;
+    notifyListeners();
+  }
+}
