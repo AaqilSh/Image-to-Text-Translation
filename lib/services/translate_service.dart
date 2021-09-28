e7ebaf4df3539371553311f1b19842a321a81024
Aaqil Shihab <aaqilsh@yahoo.com>
2021-09-28 13:37:15 +0530

    Added model download method

--- lib/services/translate_service.dart
+++ lib/services/translate_service.dart
@@ -1,3 +1,22 @@
+import 'package:google_ml_kit/google_ml_kit.dart';
+
 class TranslateService {
-  void translate() {}
+  final _translateModelManager =
+      GoogleMlKit.nlp.translateLanguageModelManager();
+
+  Future<bool> isModelAvailable(String model) async {
+    return await _translateModelManager.isModelDownloaded(model);
+  }
+
+  Future<bool> downloadModel(String text) async {
+    try {
+      await _translateModelManager.downloadModel(text, isWifiRequired: false);
+      return true;
+    } catch (e) {
+      print(e.toString());
+      return false;
+    }
+  }
+
+  void translate(String text, String langugae) {}
 }
